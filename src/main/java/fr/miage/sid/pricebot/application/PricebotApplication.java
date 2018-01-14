adb851602293c4ecfa37115ba8ba8d9d82b5e824
Alexandre Milev <alex92250@gmail.com>
2018-01-14 15:59:26 +0100

    marche pas bien

--- src/main/java/fr/miage/sid/pricebot/application/PricebotApplication.java
+++ src/main/java/fr/miage/sid/pricebot/application/PricebotApplication.java
@@ -1,12 +1,39 @@
 package fr.miage.sid.pricebot.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@ComponentScan
 @SpringBootApplication
-public class PricebotApplication {
+public class PricebotApplication  extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PricebotApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PricebotApplication.class);
+	}
+
+	private static Class<PricebotApplication> applicationClass = PricebotApplication.class;
 }
+@RestController
+class hellocontroller{
+	@RequestMapping("/hello/{name}")
+	public String hello(@PathVariable String name) {
+		// System.out.println("cooooo");
+		return name;
+	}
+}
+
+
+
--- src/main/java/fr/miage/sid/pricebot/controller/PriceController.java
+++ src/main/java/fr/miage/sid/pricebot/controller/PriceController.java
@@ -3,36 +3,36 @@ package fr.miage.sid.pricebot.controller;
 import fr.miage.sid.pricebot.service.PriceApi;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@ComponentScan
 @RestController
-@RequestMapping(value = "/priceApi")
 public class PriceController {
 
     @Autowired
     private PriceApi priceApi;
 
-    @GetMapping(value = "/price/{product}")
-    public ResponseEntity<?> getBook(@PathVariable("product") String productName) {
+
+
+    @RequestMapping(value = "/price/{product}")
+    public JSONObject getBook(@PathVariable("product") String productName) {
         JSONObject result = null;
+        System.out.println("shit");
         try {
             result = priceApi.getPrice(productName);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if(result != null) {
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return result;
     }
+
+
 
 
 }
